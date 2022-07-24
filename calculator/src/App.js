513b7d51a3b33854d366599bb7323fe6a121f712
rohit-shah840 <rohitshah1455@gmail.com>
2022-07-24 15:49:04 +0530

    calculator app

---
+++ calculator/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import "./index";
+import Calculator from "./components/calculator/Calculator";
+function App() {
+  return (
+    <div>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
